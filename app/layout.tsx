ddfb50843bc190bc1f3bcdf40b4991897cf41aec
yuli3 <861106@gmail.com>
2024-09-30 19:02:55 +0900

    0930

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,38 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAdSense } from "@/components/adsense";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'AHOXY Note, Online Notepad, Fast and Easy to use',
   description: 'AHOXY Note Simple Web-Based Free',
+  manifest: '/manifest.json',
   icons: {
-    icon: './favicon.ico',
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: [
+      { url: '/apple-icon-57x57.png', sizes: '57x57', type: 'image/png' },
+      { url: '/apple-icon-60x60.png', sizes: '60x60', type: 'image/png' },
+      { url: '/apple-icon-72x72.png', sizes: '72x72', type: 'image/png' },
+      { url: '/apple-icon-76x76.png', sizes: '76x76', type: 'image/png' },
+      { url: '/apple-icon-114x114.png', sizes: '114x114', type: 'image/png' },
+      { url: '/apple-icon-120x120.png', sizes: '120x120', type: 'image/png' },
+      { url: '/apple-icon-144x144.png', sizes: '144x144', type: 'image/png' },
+      { url: '/apple-icon-152x152.png', sizes: '152x152', type: 'image/png' },
+      { url: '/apple-icon-180x180.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'apple-touch-icon-precomposed',
+        url: '/apple-touch-icon-precomposed.png',
+      },
+    ],
   },
   openGraph: {
     title: 'AHOXY Note',
@@ -53,6 +77,7 @@ export default function RootLayout({
           >{children}
           </ThemeProvider></body>
           <GoogleAnalytics gaId="G-XX3NRGH9TQ" />
+          <GoogleAdSense />
     </html>
   );
 }
